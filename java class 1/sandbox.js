b4827732a273d79cb31a186c75abd02ed1fd4aec
Hafiz Ahmed Ali <69626544+ahmedalibehari@users.noreply.github.com>
2024-09-11 09:20:55 +0500

    Add files via upload

---
+++ java class 1/sandbox.js
@@ -0,0 +1,10 @@
+// alert-1
+alert("Hello")
+// alert-2
+alert("Error! please enter a valid password")
+// alert-3
+alert("Welcome to js land/n Happy codding!")
+// alert-4
+alert("Welcome to js land")
+// alret-5
+alert("Hello.... I can run js through my web browsers consule")
