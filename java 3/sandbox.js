0c1623924a011ddb36e187ec25c3353f5cf04d98
Hafiz Ahmed Ali <69626544+ahmedalibehari@users.noreply.github.com>
2024-09-11 11:21:34 +0500

    Add files via upload

---
+++ java 3/sandbox.js
@@ -0,0 +1,16 @@
+// alert-1
+var age =("I am 15 year old")
+alert("I am 15 year old")
+// alret-2
+var n =("break")
+alert("break\nbrea\nbre\nbr\nb\nb\nb\nb\nb\nb\nb\nb\nb\nb\nb\nb\nb\nb\nb")
+// alret-3
+var birth =("My birth year is 1990")
+alert("My birth year is 1990")
+// alert-4
+var name =("Ahmed ALi")
+alert("Ahmed ALi")
+var product =("My product is high quality")
+alert("My product is high quality")
+var perchase =("Ahmed Ali is perchaise is 5 t shirts")
+alert("Ahmed Ali is perchaise is 5 t shirts")
