dc6c70002827b38dd0eb1c1418f8a97e71ade218
Hafiz Ahmed Ali <69626544+ahmedalibehari@users.noreply.github.com>
2024-09-11 10:31:54 +0500

    Add files via upload

---
+++ java class 2/sandbox.js
@@ -0,0 +1,24 @@
+// alret-1
+var username = ("")
+// alert-2
+var myname =("Ahmed Ali")
+console.log =(myname);
+// alret-3
+var massage =("hello world")
+alert("hello world")
+// alert-4
+var name =("jhone done")
+alert("jhone done")
+var age =("15 year old")
+alert("15 year old")
+var data =("Certifite Application Development")
+alert("Certified Application Development")
+// alert-5
+var pizza =("Pizza")
+alert("Pizza\npizz\npiz\npi\np")
+// alert-6
+var Email =("My email addres is sultanahmedali865@gmail.com")
+alert("My email addres is sultanahmedali865@gmail.com")
+// alert-7
+var book =("I am trying to leran from the book A smarter  way to learn  javascript")
+alert("I am trying to lern from the book A smarter way to learn javascript")
