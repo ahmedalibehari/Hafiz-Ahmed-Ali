3a9a423f495aada62bbf2bbce6dd0f00502e7015
Hafiz Ahmed Ali <69626544+ahmedalibehari@users.noreply.github.com>
2024-10-09 17:19:22 +0500

    Add files via upload

---
+++ assigement 04/sandbox.js
@@ -0,0 +1,10 @@
+hedEl.addEventListener("click", function () {
+hedEl2.b1.toggle("success");
+});
+
+const buttonEl = document.querySelector("button1");
+buttonEl.addEventListener("click", function(){
+    console.log(clicked);
+    hedEl.b1.toggle("success")
+    
+})
